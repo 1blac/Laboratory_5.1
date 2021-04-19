3b9a6fa0cbe9e6468c29f97ff00675dfca780232
1blac <72660276+1blac@users.noreply.github.com>
2021-04-19 12:54:06 +0300

    Add files via upload

---
+++ UnitTest_5.1_A.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laboratory_5.1/Line.h"
+#include "../Laboratory_5.1/Line.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51A
+{
+	TEST_CLASS(UnitTest51A)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			LINE TEST(10, 5, 1);
+			double test = TEST.function();
+			Assert::AreEqual(15., test);
+		}
+	};
+}
