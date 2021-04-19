fce111a39b020c633bc76b8c3ff8c00dc3fd298e
1blac <72660276+1blac@users.noreply.github.com>
2021-04-19 12:54:40 +0300

    Add files via upload

---
+++ UnitTest_5.1_D.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "D:\ООП\Лабораторні 5\Laboratoty_5.1_D\Pair.h"
+#include "D:\ООП\Лабораторні 5\Laboratoty_5.1_D\Pair.cpp"
+#include "D:\ООП\Лабораторні 5\Laboratoty_5.1_D\Rational.h"
+#include "D:\ООП\Лабораторні 5\Laboratoty_5.1_D\Rational.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51D
+{
+	TEST_CLASS(UnitTest51D)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Pair a;
+			a.SetL(4);
+			Assert::AreEqual(a.GetL(), 4.);
+		}
+	};
+}
