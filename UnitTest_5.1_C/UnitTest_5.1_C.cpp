a4e822418f23553eea5b98c664f2650a2127e9db
1blac <72660276+1blac@users.noreply.github.com>
2021-04-19 12:51:32 +0300

    Add files via upload

---
+++ UnitTest_5.1_C/UnitTest_5.1_C.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Laboratory_5.1_C/Rational.h"
+#include "../Laboratory_5.1_C/Rational.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest51C
+{
+	TEST_CLASS(UnitTest51C)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			Rational a(1, 2);
+			double C = a.value();
+			Assert::AreEqual(C, 0,5);
+		}
+	};
+}
